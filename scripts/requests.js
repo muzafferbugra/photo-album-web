65466708608517dda032694633629d62a1480c78
Naci Denizhan Aydinli <daydinli93@gmail.com>
2018-11-27 22:02:21 +0300

    tum ozellikler tamamlandi

--- scripts/requests.js
+++ scripts/requests.js
@@ -50,3 +50,62 @@ function getPhotos(albumId) {
         }
     });
 }
+
+function uploadImage(albumId){
+    var fileInput = document.getElementById('file-upload-input');
+    var tags = document.getElementById('tags').value;
+    console.log(tags);
+    var documentData = new FormData();
+    documentData.append('file', fileInput.files[0]);
+    documentData.append('tags', tags);
+    
+    $.ajax({
+        url:baseUrl + "/albums/" + albumId + "/photos/" ,
+        type: 'POST',
+        data: documentData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            alert("Resim yuklendi lutfen sayfayi yenileyiniz");
+        }
+    });
+}
+
+function deleteAlbum(albumId){
+    $.ajax({
+        url:baseUrl + "/albums/" + albumId,
+        type: 'DELETE',
+        success: function (response) {
+            alert("Album silindi lutfen sayfayi yenileyiniz");
+        }
+    });
+}
+
+function deletePhoto(albumId, photoId){
+    $.ajax({
+        url:baseUrl + "/albums/" + albumId + "/photos/" + photoId,
+        type: 'DELETE',
+        success: function (response) {
+            alert("Fotograf silindi lutfen sayfayi yenileyiniz");
+        }
+    })
+}
+
+function searchPhoto() {
+    var tag = document.getElementById('searchTag').value;
+    $.ajax({
+        url:baseUrl + "/searchWithTag/" + tag ,
+        type:'GET',
+        dataType:'json',
+        contentType: "application/json; charset=utf-8",
+        success:function(data) {
+          var resim_row = document.getElementById("resim-row");
+          var resimler = "";
+          for(var i = 0; i < data.length; i++){
+            resimler += "<div class=\"gallery\"><a target=\"_blank\" href=\"" + data[i].photoLink + "\"><img src=\"" + data[i].photoLink + "\" width=\"300\" height=\"200\"></a><div class=\"desc\">" + data[i].tags + "</div></div>"
+          }
+          resim_row.innerHTML = resimler;
+        }
+    });
+}
